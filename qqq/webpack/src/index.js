7d9b2a7cf102a50f9ef2ac93c6f7ab540dd0a636
sunqiyao <347467643@qq.com>
2017-11-13 14:37:42 +0800

    a

---
+++ qqq/webpack/src/index.js
@@ -0,0 +1,3 @@
+import css from './css/index.css'
+console.log("wo");
+document.getElementById("div").innerHTML="hello world122223";
---
+++ qqq/webpack/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const Uglify = require('uglifyjs-webpack-plugin');
+module.exports = {
+    entry: {
+        entry: "./src/index.js"
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module:{
+        rules:[{
+            test: /\.css$/,
+            // use:[
+            //         {
+            //             loader:'style-loader'
+            //         },
+            //         {
+            //             loader:'css-loader'
+            //         }
+            //     ]
+            // use:['style-loader','css-loader']
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        },
+            {
+            test:/\.(png|gif|jpg)$/,
+            use:[
+            {
+                loader:'url-loader',
+                options:{
+                    limit: 300000000,
+                    outputPath:'img/'
+                }
+            }
+]}]
+
+    //加载器
+    },
+    plugins:[
+        new HtmlWebpackPlugin ({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            template:"./src/index.html"
+        }),
+        new ExtractTextPlugin('css/index.css'),
+        new Uglify()
+    ],
+    devServer:{
+        contentBase:path.resolve(__dirname,"dist"),
+        host:"127.0.0.1",
+        port:"8081",
+        compress:true
+    }
+}
