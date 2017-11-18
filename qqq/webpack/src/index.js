ef634abdc4e28025924e0f2eefe183e8758ec6d8
sunqiyao <347467643@qq.com>
2017-11-19 01:46:16 +0800

    o

--- qqq/webpack/src/index.js
+++ qqq/webpack/src/index.js
@@ -1,3 +1,7 @@
-import css from './css/index.css'
-console.log("wo");
-document.getElementById("div").innerHTML="hello world122223";
+import css from './css/index.css';
+import scss from './css/test.scss';
+document.getElementById("div1").innerHTML="sss";
+document.getElementById("div3").innerHTML="sss1";
+const bb=80;
+let aa=20;
+let fn=x=>console.log(x);
--- qqq/webpack/webpack.config.js
+++ qqq/webpack/webpack.config.js
@@ -1,61 +1,83 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const Uglify = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
     entry: {
         entry: "./src/index.js"
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist'),//"path.resolve(__dirname)"是webpack文件夹
         filename: '[name].js'
     },
-    module:{
-        rules:[{
-            test: /\.css$/,
-            // use:[
-            //         {
-            //             loader:'style-loader'
-            //         },
-            //         {
-            //             loader:'css-loader'
-            //         }
-            //     ]
-            // use:['style-loader','css-loader']
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: "css-loader"
-            })
-        },
+    module: {
+        rules: [
             {
-            test:/\.(png|gif|jpg)$/,
-            use:[
-            {
-                loader:'url-loader',
-                options:{
-                    limit: 300000000,
-                    outputPath:'img/'
-                }
-            }
-]}]
+                test: /\.js$/,
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['env']
+                    }
+                }],
+                exclude: /node_modules/
 
-    //加载器
+            },
+            {
+                test:/\.css$/,
+                // use:['style-loader','css-loader']
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    // use: "css-loader"
+                    use: [
+                        {
+                            loader:"css-loader",
+                            options:{
+                                importLoaders:1
+                            }
+                        },"postcss-loader"]
+                })
+            },
+            {
+                test: /\.scss$/,
+                // use:['style-loader','css-loader','sass-loader'],
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
+            },
+            {
+                test:/\.(png|gif|jpg)$/,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{
+                            limit: 3000000000000,
+                            outputPath:'img/'
+                        }
+                    }
+                ]
+            }
+        ]
     },
     plugins:[
-        new HtmlWebpackPlugin ({
+        new HtmlWebpackPlugin({
             minify:{
                 removeAttributeQuotes:true
             },
             hash:true,
-            template:"./src/index.html"
+            template: "./src/index.html"
         }),
-        new ExtractTextPlugin('css/index.css'),
-        new Uglify()
+            new ExtractTextPlugin('css/test.scss'),
+            new webpack.BannerPlugin("成哥所有"),
+            new ExtractTextPlugin('css/index.css')
+
+
     ],
     devServer:{
-        contentBase:path.resolve(__dirname,"dist"),
-        host:"127.0.0.1",
-        port:"8081",
+        contentBase: path.resolve(__dirname,'dist'),
+        host:'127.0.0.1',
+        port:'8081',
         compress:true
     }
 }
